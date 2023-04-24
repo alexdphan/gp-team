fd13b4493ad30c055710f09e910809040045bfa0
alexdphan <alexphan0515@gmail.com>
2023-04-24 14:38:55 -0400

    refactor ceo

--- app/ceo.py
+++ app/ceo.py
@@ -1,4 +1,6 @@
+
 from collections import deque
+import logging
 from typing import Dict, List, Optional, Any
 from pydantic import BaseModel, Field
 from langchain import LLMChain, PromptTemplate
@@ -14,8 +16,8 @@ from langchain.memory import ConversationBufferWindowMemory
 from team_member import (
     TeamMember,
     create_team_member,
-    TaskPrioritizationChain,
-    ExecutionChain,
+    # TaskPrioritizationChain,
+    # ExecutionChain,
 )
 
 # Define the Chains
@@ -29,13 +31,23 @@ class RoleCreationChain(LLMChain):
             "You are an AI tasked with creating roles for a team based on their expertise and the team's objective. Format: Role Name, Expertise Role 1, Expertise Role 2, ..."
             " User ID: {user_id}. Objective: {objective}."
             " Consider the information from the Chroma Instance: {chroma_instance}."
+            "\n\nPlease provide your response in the following format:"
+            "\nAction: [describe the action]"
+            "\nAction Input: [describe the input for the action]"
+            "\nThought: [describe the thought process]"
+            "\nFinal Answer: [provide the final answer]"
+            "\n\nExample:"
+            "\nAction: Create roles"
+            "\nAction Input: Objective, Chroma Instance"
+            "\nThought: Based on the objective and the chroma instance, the necessary roles can be..."
+            "\nFinal Answer: Role Name, Expertise Role 1, Expertise Role 2, ..."
         )
         prompt = PromptTemplate(
             template=prompt_template,
             input_variables=["user_id", "chroma_instance", "objective"],
         )
-        return cls(prompt=prompt, llm=llm, verbose=verbose)
-        
+        return cls(prompt=prompt, llm=llm, verbose=verbose) 
+       
 
 class TaskCreationAssignChain(LLMChain):
     """Chain to generate and assign tasks."""
@@ -49,6 +61,16 @@ class TaskCreationAssignChain(LLMChain):
             " Team members and their expertise roles: {team_members_expertise}."
             " Consider the information from the Chroma Instance: {chroma_instance}."
             " Create a task list for each team member and assign tasks according to their expertise."
+            "\n\nPlease provide your response in the following format:"
+            "\nAction: [describe the action]"
+            "\nAction Input: [describe the input for the action]"
+            "\nThought: [describe the thought process]"
+            "\nFinal Answer: [provide the final answer]"
+            "\n\nExample:"
+            "\nAction: Generate and assign tasks"
+            "\nAction Input: Objective, Chroma Instance, Team members and their expertise roles"
+            "\nThought: Based on the objective, chroma instance, and team members' expertise, I can create tasks that align with their skills..."
+            "\nFinal Answer: Task list for each team member"
         )
         prompt = PromptTemplate(
             template=task_assignment_template,
@@ -60,14 +82,27 @@ class TaskCreationAssignChain(LLMChain):
             ],
         )
         return cls(prompt=prompt, llm=llm, verbose=verbose)
+
         
-    # used to help the chain generate tasks for each team member based on their expertise, the objective, and the Chroma instance
     def generate_new_task_list(self, vectorstore: Chroma, result: Dict, task_description: str, task_list: List[str], objective: str, top_k: int) -> List[Dict]:
-        """Get the next task."""
+        """
+        Get the next task based on team members' expertise, the objective, and the Chroma instance.
+
+        Args:
+            vectorstore (Chroma): A Chroma instance containing information to consider.
+            result (Dict): A dictionary containing relevant information about the current state.
+            task_description (str): A description of the task to generate.
+            task_list (List[str]): A list of existing tasks.
+            objective (str): The team's objective.
+            top_k (int): The number of top tasks to consider.
+
+        Returns:
+            List[Dict]: A list of dictionaries, each containing a task name.
+        """
         top_tasks = self.get_top_tasks(vectorstore, task_description, top_k)
         task_list.extend(top_tasks)
         incomplete_tasks = ", ".join(task_list)
-    
+
         response = self.run(
             result=result,
             task_description=task_description,
@@ -75,32 +110,37 @@ class TaskCreationAssignChain(LLMChain):
             objective=objective,
         )
         # response is a string, so we split it into a list of strings
-        new_tasks = response.split("\n")
+        new_tasks = response.split(",")  # Use a more specific delimiter
         # return a list of dictionaries, each dictionary has a task_name key value being the task name
-        return [{"task_name": task_name} for task_name in new_tasks if task_name.strip()]
+        return [{"task_name": task_name.strip()} for task_name in new_tasks if task_name.strip()]
+
     
     # used to help the chain generate tasks for each team member based on their expertise, the objective, and the Chroma instance
     # used within the generate_new_task_list function
     def get_top_tasks(self, vectorstore: Chroma, query: str, k: int) -> List[str]:
-        """Get the top k tasks based on the query. This is used when the CEO is creating a new task list, getting the top k tasks from the Chroma instance as a starting point. Important because the CEO is not an expert in the field, so the CEO needs to rely on the Chroma instance to get the top k tasks."""
-        try:
-            results = vectorstore.similarity_search_with_score(query=query, k=k)
-        except chromadb_errors.NoIndexException:
-            return []
-        if not results:
-            return []
-
-        sorted_results, _ = zip(*sorted(results, key=lambda x: x[1], reverse=True))
-
-        tasks = []
-        for item in sorted_results:
+            """
+            Get the top k tasks based on the query. This is used when the CEO is creating a new task list,
+            getting the top k tasks from the Chroma instance as a starting point. Important because the CEO
+            is not an expert in the field, so the CEO needs to rely on the Chroma instance to get the top k tasks.
+            """
             try:
-                tasks.append(str(item.metadata["task"]))
-            except KeyError:
-                # Add a relevant message or remove the print statement if it's not necessary
-                print(f"")
+                results = vectorstore.similarity_search_with_score(query=query, k=k)
+            except chromadb_errors.NoIndexException:
+                return []
+            if not results:
+                return []
 
-        return tasks
+            sorted_results, _ = zip(*sorted(results, key=lambda x: x[1], reverse=True))
+
+            tasks = []
+            for item in sorted_results:
+                try:
+                    tasks.append(str(item.metadata["task"]))
+                except KeyError:
+                    # Log a relevant message or remove the logging statement if it's not necessary
+                    logging.warning(f"KeyError occurred while processing the metadata for item: {item}")
+
+            return tasks
 
 class ReportCreationChain(LLMChain):
     """Chain to generate a report."""
@@ -114,6 +154,10 @@ class ReportCreationChain(LLMChain):
             " Team members and their expertise roles: {team_members_expertise}."
             " Consider the information from the Chroma Instance: {chroma_instance}."
             " Please update the report with the latest information."
+            "\n\nAction: {{action}}"
+            " Action Input: {{action_input}}"
+            " Thought: {{thought}}"
+            " Final Answer: {{final_answer}}"
         )
         prompt = PromptTemplate(
             template=prompt_template,
@@ -134,6 +178,10 @@ class ReviseCreationChain(LLMChain):
             " Consider the information from the Chroma Instance: {chroma_instance}."
             " If there are no outputs to revise, provide only what the you have done so far."
             " The user provided the following feedback: {user_feedback}. With this, please revise the outputs accordingly."
+            "\n\nAction: {{action}}"
+            " Action Input: {{action_input}}"
+            " Thought: {{thought}}"
+            " Final Answer: {{final_answer}}"
             )
         prompt = PromptTemplate(
         template=prompt_template,
@@ -145,10 +193,10 @@ class CEO:
     def __init__(
         self,
         chroma_instance: Chroma,
-        role_creation_chain: RoleCreationChain,
-        task_creation_assign_chain: TaskCreationAssignChain,
-        report_creation_chain: ReportCreationChain,
-        revise_creation_chain: ReviseCreationChain,
+        role_creation_chain: LLMChain,
+        task_creation_assign_chain: LLMChain,
+        report_creation_chain: LLMChain,
+        revise_creation_chain: LLMChain,
     ):
         self.chroma_instance = chroma_instance
         self.role_creation_chain = role_creation_chain
@@ -158,25 +206,21 @@ class CEO:
         self.team_members: Dict[str, TeamMember] = {}
         self.team_member_outputs = {}
         self.user_id = 0
-        self.user_feedback = ""  # Initialize the user_feedback variable
+        self.user_feedback = ""
 
-        # Initialize the chains for the team members
-        self.task_prioritization_chain = TaskPrioritizationChain.from_llm(role_creation_chain.llm)
-        self.execution_chain = ExecutionChain.from_llm(revise_creation_chain.llm)
-
+        self.task_prioritization_chain = LLMChain.from_llm(role_creation_chain.llm)
+        self.execution_chain = LLMChain.from_llm(revise_creation_chain.llm)
 
     def get_new_user_id(self):
         self.user_id += 1
         return f"user_{self.user_id}"
-        
+
     def create_team_members(self, objective, num_team_members):
-        # Run the RoleCreationChain to generate roles for the team members
         roles = self.role_creation_chain.run(
             user_id=self.user_id,
             objective=objective,
             chroma_instance=self.chroma_instance,
         )
-
         expertise_roles = roles.strip().split("\n")[:num_team_members]
 
         for i, role in enumerate(expertise_roles):
@@ -184,17 +228,13 @@ class CEO:
             team_member = create_team_member(
                 user_id=team_member_id,
                 expertise_role=role,
-                task_list=[],  # Empty task list for now, will be assigned later
+                task_list=[],
                 task_prioritization_chain=self.task_prioritization_chain,
                 execution_chain=self.execution_chain,
                 objective=objective,
-            )
+            ) 
             self.team_members[team_member_id] = team_member
-
-        print("Roles:")
-        print(roles)
-        print("Expertise Roles:")
-        print(expertise_roles)
+    
 
     def assign_tasks_to_team_members(self, objective: str):
         team_members_expertise = self.get_team_members_expertise()
@@ -205,64 +245,61 @@ class CEO:
             chroma_instance=self.chroma_instance,
             team_members_expertise=team_members_expertise,
         )
+        task_lists = ListParser.parse(task_lists)
 
-        # Assuming the output is a list of lists with tasks for each team member
         for user_id, task_list in zip(self.team_members.keys(), task_lists):
             self.team_members[user_id].task_list = task_list
+            
+        class ListParser:
+            @staticmethod
+            def parse(input_str: str, delimiter: str = ";", item_separator: str = ","):
+                result = []
+                groups = input_str.split(delimiter)
+                for group in groups:
+                    result.append(group.split(item_separator))
+                return result
 
+
+    def get_team_members_expertise(self):
+        return {user_id: team_member.expertise_role for user_id, team_member in self.team_members.items()}
+        # reutrns a dictionary of user_id: expertise_role for each team member, this is generated from the role creation chain
 
     def execute_chains(self, objective: str, num_team_members: int, user_feedback: str):
-        # Rest of the function implementation remains the same
         self.create_team_members(objective=objective, num_team_members=num_team_members)
         self.assign_tasks_to_team_members(objective=objective)
 
         team_outputs = {}
         for user_id, team_member in self.team_members.items():
-            # Prioritize tasks for each team member
             prioritized_task_list = team_member.prioritize_tasks(chroma_instance=self.chroma_instance)
             team_member.task_list = prioritized_task_list
-
-            print(f"Executing tasks for team member {user_id}...")
-            # Execute tasks for each team member and store the outputs
             team_outputs[user_id] = team_member.execute_tasks(chroma_instance=self.chroma_instance)
-            print(f"Team Member {user_id} outputs: {team_outputs[user_id]}")
 
         report = self.report_creation_chain.run(
             user_id=self.user_id,
             objective=objective,
             chroma_instance=self.chroma_instance,
-            team_members_expertise={
-                user_id: team_member.expertise_role
-                for user_id, team_member in self.team_members.items()
-            },
+            team_members_expertise=self.get_team_members_expertise(),
         )
 
+        
         revised_team_outputs = self.revise_creation_chain.run(
-                    {
-                        'user_id': self.user_id,
-                        'objective': objective,
-                        'chroma_instance': self.chroma_instance,
-                        'team_members_expertise': {
-                            user_id: team_member.expertise_role
-                            for user_id, team_member in self.team_members.items()
-                        },
-                        'user_feedback': self.user_feedback,  # Pass the user_feedback attribute from the CEO object
-                    }
-                )
-        print(f"Report: {report}")
-        print(f"Revised Team Outputs: {revised_team_outputs}")
+        {
+            'user_id': self.user_id,
+            'objective': objective,
+            'chroma_instance': self.chroma_instance,
+            'team_members_expertise': self.get_team_members_expertise(),
+            'user_feedback': self.user_feedback,
+        }
+    )
 
         return {
             "report": report,
             "revised_team_outputs": revised_team_outputs,
-            "team_member_outputs": self.team_member_outputs,
+            "team_member_outputs": team_outputs,
         }
-
-
 
     def receive_output(self, team_member_id, output):
         self.team_member_outputs[team_member_id] = output
-
 
     def get_team_members_expertise(self):
         team_members_expertise = {}
@@ -270,49 +307,25 @@ class CEO:
             team_members_expertise[team_member_id] = team_member_instance.expertise_role
         return team_members_expertise
 
-
     def handle_feedback(self, feedback: str):
         self.user_feedback = feedback  # Set the user_feedback attribute on the CEO object
 
+
     ########## Workflow ##########
     def run_workflow(self, objective: str, num_team_members):
-        # global user_feedback
-        user_feedback = ""
-
         print("Creating team members...")
         self.create_team_members(objective=objective, num_team_members=num_team_members)
         print("Assigning tasks to team members...")
         self.assign_tasks_to_team_members(objective=objective)
         print("Executing chains...")
-        results = self.execute_chains(objective=objective, num_team_members=num_team_members, user_feedback=user_feedback)
+        results = self.execute_chains(objective=objective, num_team_members=num_team_members, user_feedback=self.user_feedback)
         report = results["report"]
         revised_team_outputs = results["revised_team_outputs"]
         print("Workflow complete!")
 
-        # Start looping through 2-6
-        while True:
-            # Check if the user wants to provide feedback or exit the loop
-            user_input = input("Type 'feedback' to provide feedback or 'exit' to stop the workflow: ").strip().lower()
-            if user_input == "feedback":
-                user_feedback = input("Please provide your feedback: ")
-            elif user_input == "exit":
-                break
-            else:
-                print("Invalid input. Please type 'feedback' or 'exit'.")
-                continue
-
-            print("Assigning tasks to team members...")
-            self.assign_tasks_to_team_members(objective=objective)
-            print("Executing chains...")
-            results = self.execute_chains(objective=objective, num_team_members=num_team_members)
-            report = results["report"]
-            revised_team_outputs = results["revised_team_outputs"]
-
-        # Return the final report and revised team outputs for further use
         return report, revised_team_outputs
 
-# somethign commint
-# this is for the user to interact with the system
+
 class UserMessageHandler:
     def __init__(self, ceo: CEO):
         self.ceo = ceo
@@ -349,4 +362,3 @@ class UserMessageHandler:
     async def handle_input(self, user_input: str) -> str:
         response = self.process_message(user_input)
         return response
-    
--- app/main.py
+++ app/main.py
@@ -1,82 +1,82 @@
-import os
-from typing import Optional
-from dotenv import load_dotenv
-from fastapi import FastAPI, Depends, HTTPException
-from pydantic import BaseModel
-from langchain.llms.openai import OpenAI
-from langchain.vectorstores.chroma import Chroma
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.memory import ConversationBufferWindowMemory
-from ceo import CEO, RoleCreationChain, TaskCreationAssignChain, ReportCreationChain, ReviseCreationChain, UserMessageHandler
-from fastapi.middleware.cors import CORSMiddleware
-from ceo import CEO, RoleCreationChain, TaskCreationAssignChain, ReportCreationChain, ReviseCreationChain, UserMessageHandler
+# import os
+# from typing import Optional
+# from dotenv import load_dotenv
+# from fastapi import FastAPI, Depends, HTTPException
+# from pydantic import BaseModel
+# from langchain.llms.openai import OpenAI
+# from langchain.vectorstores.chroma import Chroma
+# from langchain.embeddings import OpenAIEmbeddings
+# from langchain.memory import ConversationBufferWindowMemory
+# from ceo import CEO, RoleCreationChain, TaskCreationAssignChain, ReportCreationChain, ReviseCreationChain, UserMessageHandler
+# from fastapi.middleware.cors import CORSMiddleware
+# from ceo import CEO, RoleCreationChain, TaskCreationAssignChain, ReportCreationChain, ReviseCreationChain, UserMessageHandler
 
-# Set Variables
-load_dotenv()
+# # Set Variables
+# load_dotenv()
 
-# Read API keys from environment variables
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+# # Read API keys from environment variables
+# OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
-# Define your embedding model
-embeddings_model = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+# # Define your embedding model
+# embeddings_model = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
-YOUR_TABLE_NAME = os.getenv("TABLE_NAME", "")
-assert YOUR_TABLE_NAME, "TABLE_NAME environment variable is missing from .env"
-table_name = YOUR_TABLE_NAME
+# YOUR_TABLE_NAME = os.getenv("TABLE_NAME", "")
+# assert YOUR_TABLE_NAME, "TABLE_NAME environment variable is missing from .env"
+# table_name = YOUR_TABLE_NAME
 
-persist_directory = "db"
+# persist_directory = "db"
 
-chroma_instance = (
-    Chroma(table_name, embeddings_model,
-           persist_directory=persist_directory)
-)
+# chroma_instance = (
+#     Chroma(table_name, embeddings_model,
+#            persist_directory=persist_directory)
+# )
 
-# Initialize LLM instance with the API key
-llm = OpenAI(api_key=OPENAI_API_KEY)
+# # Initialize LLM instance with the API key
+# llm = OpenAI(api_key=OPENAI_API_KEY)
 
-# Initialize the CEO module and all chains
-role_creation_chain = RoleCreationChain.from_llm(llm)
-task_creation_assign_chain = TaskCreationAssignChain.from_llm(llm)
-report_creation_chain = ReportCreationChain.from_llm(llm)
-revise_creation_chain = ReviseCreationChain.from_llm(llm)
+# # Initialize the CEO module and all chains
+# role_creation_chain = RoleCreationChain.from_llm(llm)
+# task_creation_assign_chain = TaskCreationAssignChain.from_llm(llm)
+# report_creation_chain = ReportCreationChain.from_llm(llm)
+# revise_creation_chain = ReviseCreationChain.from_llm(llm)
 
-app = FastAPI()
+# app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
+# origins = [
+#     "http://localhost",
+#     "http://localhost:3000",
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-class InputModel(BaseModel):
-    user_input: Optional[str] = None
-    objective: Optional[str] = None
-    num_team_members: Optional[int] = None
-    feedback: Optional[str] = None
+# class InputModel(BaseModel):
+#     user_input: Optional[str] = None
+#     objective: Optional[str] = None
+#     num_team_members: Optional[int] = None
+#     feedback: Optional[str] = None
 
-async def get_ceo() -> CEO:
-    # Your existing CEO initialization code
-    ceo = CEO(
-        chroma_instance=chroma_instance,
-        role_creation_chain=role_creation_chain,
-        task_creation_assign_chain=task_creation_assign_chain,
-        report_creation_chain=report_creation_chain,
-        revise_creation_chain=revise_creation_chain,
-    )
-    return ceo
+# async def get_ceo() -> CEO:
+#     # Your existing CEO initialization code
+#     ceo = CEO(
+#         chroma_instance=chroma_instance,
+#         role_creation_chain=role_creation_chain,
+#         task_creation_assign_chain=task_creation_assign_chain,
+#         report_creation_chain=report_creation_chain,
+#         revise_creation_chain=revise_creation_chain,
+#     )
+#     return ceo
 
-@app.post("/process")
-async def process_inputs(input_data: InputModel, ceo: CEO = Depends(get_ceo)):
-    user_input = input_data.user_input.strip()
-    if not user_input:
-        raise HTTPException(status_code=400, detail="Invalid input")
+# @app.post("/process")
+# async def process_inputs(input_data: InputModel, ceo: CEO = Depends(get_ceo)):
+#     user_input = input_data.user_input.strip()
+#     if not user_input:
+#         raise HTTPException(status_code=400, detail="Invalid input")
 
-    response = await ceo.run_workflow(user_input)
-    return {"response": response}
+#     response = await ceo.run_workflow(user_input)
+#     return {"response": response}
